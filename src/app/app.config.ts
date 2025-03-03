aa4fe48b52f5f6f0169ea9c1cf211d310fc207be
nathan-moura-brisa <natan.moura@grupobrisanet.com.br>
2025-03-03 12:41:33 -0300

    First commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,13 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { getSingleSpaExtraProviders } from 'single-spa-angular';
+import { EmptyRouteComponent } from './empty-route/empty-route.component';
+import { APP_BASE_HREF } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [getSingleSpaExtraProviders(),
+    provideRouter([{path: '**', component: EmptyRouteComponent }]),
+    {provide: APP_BASE_HREF, useValue: '/'}
+  ]
 };
---
+++ src/main.single-spa.ts
@@ -0,0 +1,24 @@
+import { enableProdMode, NgZone } from '@angular/core';
+import { Router, NavigationStart } from '@angular/router';
+
+import { singleSpaAngular } from 'single-spa-angular';
+
+import { AppComponent } from './app/app.component';
+import { appConfig } from './app/app.config';
+import { bootstrapApplication } from '@angular/platform-browser';
+
+// if (environment.production) {
+//   enableProdMode();
+// }
+
+const lifecycles = singleSpaAngular({
+  bootstrapFunction: singleSpaProps => {
+    return bootstrapApplication(AppComponent, appConfig);
+  },
+  template: '<app-root />',
+  NgZone,
+});
+
+export const bootstrap = lifecycles.bootstrap;
+export const mount = lifecycles.mount;
+export const unmount = lifecycles.unmount;
